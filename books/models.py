c650da46a280e868bfd4d6f595853ee7fe82a4e8
Claire Fuenning <cfuenning1@gmail.com>
2025-10-15 23:44:19 -0500

    updated html files and settings

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models #imports ORM module
 
-# Create your models here.
+#defines table named score 
+#uses models.Model which is base class for all Django models
+class Score(models.Model): 
+
+    #each attribute represents field in table
+    name = models.CharField(max_length=100)
+    value = models.IntegerField() 
+    
+    def __str__(self): 
+        return self.name
