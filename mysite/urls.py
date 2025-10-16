65024f2cecbdd9a4d768070dfc245d01acf4f6b2
Claire Fuenning <cfuenning1@gmail.com>
2025-10-15 21:55:57 -0500

    comment out import statement for testing

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from books.views import hello
+#from books.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
