33573d8d64e1b73576df332405f3ed65d84f83a4
Claire Fuenning <cfuenning1@gmail.com>
2025-10-16 01:11:08 -0500

    update import statements

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import Score
 
 #The render function generates HTTP response
 #These functions handle HTTP requests and return HTTP response
