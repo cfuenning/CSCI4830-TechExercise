97a9906285eccbe1b839434c733d9c626d731e84
Claire Fuenning <cfuenning1@gmail.com>
2025-10-15 21:47:05 -0500

    added templates and html files per class examples

--- books/views.py
+++ books/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+
+#The render function generates HTTP response
+#These functions handle HTTP requests and return HTTP response
+def index(request): 
+    return render(request, 'index_hello.html') 
+
+def about(request):
+    return render(request, 'about_hello.html')
+
+'''
+SUPER SIMPLE CODE TO TEST IF SERVER WORKED:
+
 from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse("Hello, world!!!")
+'''
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from books.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello), #Maps the root URL to the 'hello' view
+    path('', include('books.urls')), #Maps the root URL to the books app urls
 ]
